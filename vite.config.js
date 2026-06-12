b92ceac386de4a77d5f35b93f4d6df9134d4558c
vesh8750 <vesh8750@gmail.com>
2026-06-13 00:20:36 +0530

    Add build configuration with multiple HTML entry points in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [tailwindcss()]
+  plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        page1: resolve(__dirname, 'page-1.html'),
+        page2: resolve(__dirname, 'page-2.html'),
+        page2Detail: resolve(__dirname, 'page-2-detail.html'),
+        page3: resolve(__dirname, 'page-3.html'),
+        page3Detail: resolve(__dirname, 'page-3-detail.html'),
+        page4: resolve(__dirname, 'page-4.html'),
+        page5: resolve(__dirname, 'page-5.html'),
+        page6: resolve(__dirname, 'page-6.html'),
+      }
+    }
+  }
 })
